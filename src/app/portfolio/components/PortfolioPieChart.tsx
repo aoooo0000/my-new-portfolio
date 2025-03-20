aa603dcc8ca51c5edb077b9a00068118740ade0f
aoooo0000 <k3329985@gmail.com>
2025-03-20 15:47:24 +0800

    fix recharts tooltip type error

--- src/app/portfolio/components/PortfolioPieChart.tsx
+++ src/app/portfolio/components/PortfolioPieChart.tsx
@@ -40,7 +40,7 @@ export default function PortfolioPieChart() {
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
           </Pie>
-          <Tooltip formatter={(value) => `$${value.toFixed(2)}`} />
+          <Tooltip formatter={(value) => `$${Number(value).toFixed(2)}`} />
         </PieChart>
       </ResponsiveContainer>
     </div>
